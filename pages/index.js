ab61dcdf80626519e53f30bb12a202289b49d20f
maxlar01 <maxlar.noctis@gmail.com>
2023-01-11 00:49:52 +0800

    Integrated Chakra UI. Working on Navigation Bar..

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,22 @@
+import { Container, Box, Heading } from '@chakra-ui/react'
+
 const Page = () => {
-    return <div>Hello</div>
+  return (
+    <Container>
+      <Box borderRadius="lg" p={3} mb={6} align="center">
+        Hi, I&apos;m a full-stack engineer based in Malaysia!
+      </Box>
+
+      <Box display={{ md: 'flex' }}>
+        <Box flexGrow={1}>
+          <Heading as="h2" variant="page-title">
+            Mostafa Rakha
+          </Heading>
+          <p>Full-Stack Engineer</p>
+        </Box>
+      </Box>
+    </Container>
+  )
 }
 
 export default Page
