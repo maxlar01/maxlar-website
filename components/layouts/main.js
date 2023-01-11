251886b0f55265ab873e2046e7c5939d4ec92b15
maxlar01 <maxlar.noctis@gmail.com>
2023-01-11 13:46:36 +0800

    code reformat..

--- components/layouts/main.js
+++ components/layouts/main.js
@@ -6,6 +6,10 @@ const Main = ({ children, router }) => {
     <Box as="main" pb={8}>
       <Head>
         <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <link
+          rel="icon"
+          href="https://emojipedia-us.s3.dualstack.us-west-1.amazonaws.com/thumbs/120/microsoft/319/desktop-computer_1f5a5-fe0f.png"
+        />
         <title>Mostafa Rakha - Homepage</title>
       </Head>
 
